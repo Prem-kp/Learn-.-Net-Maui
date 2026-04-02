0dd47fc7b79f276428873b21a010f49d5a0fbb65
PremKumar <PremKumar@LAPTOP-B6JIS2Q8>
2026-04-02 11:48:09 +0530

    use page navigation

--- Shell Navigation/Shell Navigation/pages/Page2.xaml.cs
+++ Shell Navigation/Shell Navigation/pages/Page2.xaml.cs
@@ -6,4 +6,9 @@ public partial class Page2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+Navigation.PushAsync(new Page3());
+    }
 }
